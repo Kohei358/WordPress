1d27ad50ca32210e31fccd7886331701d694213f
Kohei_Fujimura <kohei.fujimura.35@progummy.com>
2024-03-07 23:28:07 +0100

    commit on 7th Mar

--- samurai_clinic/script.js
+++ samurai_clinic/script.js
@@ -1,7 +1,12 @@
-$(function () {
-  // ボタンアニメーション
-  $('#btn_02').on('mouseover', function () {
-    console.log("test");
-  });
-});
+// $('#btn_02').hover(() => {
+//   $('#btn_02').html("<a href='about.html'><img src='images/btn_02-1.png' alt='' id='btn_02-1'></a>");
+// }, () => {
+//   $('#btn_02').html("<img src='images/btn_02.png' alt='' id='btn_02'>");
+// });
 
+$('#btn_02').on("mouseover", () => {
+  $('#btn_02').html("<a href='about.html'><img src='images/btn_02-1.png' alt='' id='btn_02-1'></a>");
+});
+$('#btn_02-1').on("mouseleave", () => {
+  $('#btn_02').html("<img src='images/btn_02.png' alt='' id='btn_02'>");
+});
