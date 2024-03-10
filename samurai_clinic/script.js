981d716a4e0aa383bf0c0fc1218432406aae91b4
Kohei_Fujimura <kohei.fujimura.35@progummy.com>
2024-03-10 01:29:56 +0100

    commit on 9th March

--- samurai_clinic/script.js
+++ samurai_clinic/script.js
@@ -4,9 +4,15 @@
 //   $('#btn_02').html("<img src='images/btn_02.png' alt='' id='btn_02'>");
 // });
 
+const targetImg = document.querySelector("#img")
+
 $('#btn_02').on("mouseover", () => {
-  $('#btn_02').html("<a href='about.html'><img src='images/btn_02-1.png' alt='' id='btn_02-1'></a>");
+  // console.log("mouse over");
+  // $('#btn_02').html("<a href='about.html'><img src='images/btn_02-1.png' alt=''></a>");
+  targetImg.src = 'images/btn_02-1.png';
 });
-$('#btn_02-1').on("mouseleave", () => {
-  $('#btn_02').html("<img src='images/btn_02.png' alt='' id='btn_02'>");
+$('#btn_02').on("mouseleave", () => {
+  // console.log("mouse leave");
+  // $('#btn_02').html("<a href='about.html'><img src='images/btn_02.png' alt=''></a>");
+  targetImg.src = 'images/btn_02.png';
 });
