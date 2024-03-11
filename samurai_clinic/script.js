cd8c0a36bab49636d1bdacc2f72b20e2f69e8527
Kohei_Fujimura <kohei.fujimura.35@progummy.com>
2024-03-11 22:09:02 +0100

    commit on 11th March

--- samurai_clinic/script.js
+++ samurai_clinic/script.js
@@ -1,18 +1,34 @@
-// $('#btn_02').hover(() => {
-//   $('#btn_02').html("<a href='about.html'><img src='images/btn_02-1.png' alt='' id='btn_02-1'></a>");
-// }, () => {
-//   $('#btn_02').html("<img src='images/btn_02.png' alt='' id='btn_02'>");
-// });
-
 const targetImg = document.querySelector("#img")
 
 $('#btn_02').on("mouseover", () => {
-  // console.log("mouse over");
-  // $('#btn_02').html("<a href='about.html'><img src='images/btn_02-1.png' alt=''></a>");
   targetImg.src = 'images/btn_02-1.png';
 });
 $('#btn_02').on("mouseleave", () => {
-  // console.log("mouse leave");
-  // $('#btn_02').html("<a href='about.html'><img src='images/btn_02.png' alt=''></a>");
   targetImg.src = 'images/btn_02.png';
 });
+
+const menu = document.querySelector(".top-nav-sp");
+menu.classList.remove("showMenu");
+const toggleBtn = document.querySelector("#toggleBtn");
+const closeBtn = document.querySelector("#closeBtn");
+
+toggleBtn.addEventListener("click", e => toggleMenu(e));
+closeBtn.addEventListener("click",e=>closeMenu(e));
+
+console.log(menu);
+function toggleMenu(e) {
+  if(e.target.id == "toggleBtn"){
+    if (menu.classList.contains("showMenu")) {
+      menu.classList.remove("showMenu");
+      menu.style.display = "none";
+    }
+    else {
+      menu.classList.add("showMenu");
+      menu.style.display = "block";
+    }
+  }
+}
+function closeMenu(e) {
+  menu.classList.remove("showMenu");
+  menu.style.display = "none";
+}
